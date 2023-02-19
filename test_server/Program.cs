1c01e92114bf2c8062460c4982d4821ae080068e
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-02-19 12:23:56 +0700

    add 2d array

--- test_server/Program.cs
+++ test_server/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// GET THE LENGTH OD 2D ARRAY
+int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        Console.WriteLine(numbers[i, j]);
+    }
+}
